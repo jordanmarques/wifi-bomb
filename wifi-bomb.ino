3de2b41da8a01b2a8b085bc7092562035a856912
Jordan Marques <jordanmarques1994@gmail.com>
2017-03-16 15:42:06 +0100

    setup led and blink when attack is in progress

--- wifi-bomb.ino
+++ wifi-bomb.ino
@@ -8,11 +8,14 @@ uint8_t pkt[1000];
 
 void setup() {
   Serial.begin(115200);
+
+  setupLED();
   setupWifi();
   memset(pkt, 0, sizeof(pkt));
 }
 
 void loop() {
+  digitalWrite(LED_BUILTIN, LOW);
   WiFiUDP udp;
   Serial.println("Send package");
   udp.beginPacketMulticast(WiFi.softAPIP(), 1, IPAddress(255,255,255,255), 1);
@@ -29,4 +32,12 @@ void setupWifi() {
     
     Serial.println("Local ip");
     Serial.println(WiFi.localIP());
+}
+
+void setupLED(){
+  Serial.println("");
+  Serial.println("Configuring LED");
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("LED Configured");
 }
